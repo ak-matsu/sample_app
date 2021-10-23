e2983ff4ad4c7159c32ad93d7c6bc948a898e1e4
ak-matsu <sandbox.matsu@gmail.com>
2021-10-23 04:24:01 +0000

    return

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user or user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       # エラーメッセージを作成する
