17993a633825ba80cefa348d2a5bf848513bb773
ak-matsu <sandbox.matsu@gmail.com>
2021-11-08 13:10:48 +0000

    list8.27:メールアドレスが正しくてパスワードが誤っている場合をテストする

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -9,8 +9,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email:    （コードを書き込む）,
-                                          password: "invalid" } }
+    post login_path, params: { session: { email:"",password:""} }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
