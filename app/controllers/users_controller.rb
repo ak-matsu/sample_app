7ffd2abd8a91674436519e6b89e628b184666770
ak-matsu <sandbox.matsu@gmail.com>
2021-11-09 03:22:05 +0000

    list8.29:ユーザー登録中にログインする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      
-      # redirect_to user_url(@user) この省略が下記コード
       redirect_to @user
-      # 保存の成功をここで扱う
-      
     else
       render 'new'
     end
   end
-  
   
   private
   
