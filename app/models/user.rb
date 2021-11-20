b9fd9ebfdbadf0f30662f85816940742aa8b1639
ak-matsu <sandbox.matsu@gmail.com>
2021-11-20 06:18:22 +0000

    リスト 9.5:class << selfを使ってdigestとnew_tokenメソッドを定義する

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,25 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
-  # 渡された文字列のハッシュ値を返す
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
   
-  #ランダムなトークンを返す
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-  
-  #永続セッションのためにユーザーをデータベースに記憶する
-  def remember_token
-    self.remember_token=User.new_token
-    update_attribute(:remember_digest,User.digest(remember_token
-    ))
+  class << self
+    # 渡された文字列のハッシュ値を返す
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    #ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    #永続セッションのためにユーザーをデータベースに記憶する
+    def remember_token
+      self.remember_token=User.new_token
+      update_attribute(:remember_digest,User.digest(remember_token
+      ))
+    end
   end
 end
