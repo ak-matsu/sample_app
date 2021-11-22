3bc73f3cb6ba0cbbda22c3aa8c4f4091750975e6
ak-matsu <sandbox.matsu@gmail.com>
2021-11-22 06:07:18 +0000

    リスト 9.7:ログインしてユーザーを保持する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
