aee95b845182eb99d6422298e220e124199a401f
ak-matsu <sandbox.matsu@gmail.com>
2021-11-23 07:29:14 +0000

    update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,32 +1,32 @@
 module SessionsHelper
-  
-  #渡されたユーザーでログインする
+
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  #ユーザーのセッションを永続的にする
+
+  # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
-  # 現在ログイン中のユーザーを返す（いる場合）
+
+  # 現在ログインしているユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id:session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
-  
-  #ユーザーがログインしていればtrue,その他ならfalseを返す
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-   
-  #現在のユーザーをログアウトする
+
+  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
-    @current_user
+    @current_user = nil
   end
 end
