45bb793a1fad7a709b149d3570ed3d4e2a6463c0
ak-matsu <sandbox.matsu@gmail.com>
2022-01-04 07:10:11 +0000

    type_miss

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -41,16 +41,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
   
   test "login with remembering" do
-    log_in_as(@user,remember_me:'1')
+    log_in_as(@user, remember_me: '1')
     assert_not_empty cookies[:remember_token]
   end
-  
+
   test "login without remembering" do
     # cookieを保存してログイン
-    log_in_as(@user,remember_me:'1')
+    log_in_as(@user, remember_me: '1')
     delete logout_path
     # cookieを削除してログイン
-    log_in_as(@user,remember_me:'0')
-    assert_not_empty cookies[:remember_token]
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end
