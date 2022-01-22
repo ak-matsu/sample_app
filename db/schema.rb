2c473e8ec562a3e115d51a49a4bff6c294cc5b22
ak-matsu <sandbox.matsu@gmail.com>
2022-01-22 05:55:12 +0000

    リスト 10.54:boolean型のadmin属性をUserに追加するマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_113340) do
+ActiveRecord::Schema.define(version: 2022_01_22_055019) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_113340) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
