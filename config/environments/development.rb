11e5cff3faa0c90dec9764dd3bb865803ca3ef82
ak-matsu <sandbox.matsu@gmail.com>
2022-02-13 05:36:47 +0000

    hostname 変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = 'https://ap-northeast-1.console.aws.amazon.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  host = 'ap-northeast-1.console.aws.amazon.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
   # クラウドIDEの場合は以下をお使いください
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # localhostで開発している場合は以下をお使いください
