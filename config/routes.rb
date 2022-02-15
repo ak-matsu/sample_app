f616bc4661277727e93c851a2aefbde0681a76fb
ak-matsu <sandbox.matsu@gmail.com>
2022-02-15 04:19:41 +0000

    type_miss is account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -10,5 +8,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  resources :acount_activations, only: [:edit]
+  resources :account_activations, only: [:edit]
 end
