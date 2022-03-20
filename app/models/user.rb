ea40a6eea0a53f68df7b4f59904225be6b1c54eb
ak-matsu <sandbox.matsu@gmail.com>
2022-03-20 12:36:38 +0000

    リスト 12.20:update_columnsを使用するテンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token,  :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -42,8 +43,7 @@ class User < ApplicationRecord
   
   # アカウントを有効にする
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
@@ -54,8 +54,7 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at:Time.zone.now)
   end
   
   # パスワード再設定のメールを送信する
